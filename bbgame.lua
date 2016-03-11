9bdf05db62181bd219e7ce41e44c5f81ecb999d7
murillow <murdrof@gmail.com>
2016-03-11 16:58:46 -0300

    Added clear console feature (Unix)
    No indentation changes

--- bbgame.lua
+++ bbgame.lua
@@ -4,14 +4,14 @@ local inf
 local sup
 
 repeat
-    print("Entre com um número")
-    inf = io.read()
-    print("Entre com outro número")
-    sup = io.read()
+   print("Entre com um número")
+   inf = io.read()
+   print("Entre com outro número")
+   sup = io.read()
 
-    if (sup - inf) < 20 then
-        print("Segundo - Primeiro deve ser >= 20")
-    end
+   if (sup - inf) < 20 then
+      print("Segundo - Primeiro deve ser >= 20")
+   end
 until (sup - inf) >= 20
 
 math.randomseed(os.time())
@@ -24,25 +24,25 @@ local maximo = math.floor(math.log(sup - inf)/math.log(2))
 local palpite
 
 while tentativas <= maximo do
-    print("Tente adivinhar o numero no qual pensei")
+   print("Tente adivinhar o numero no qual pensei")
 
-    tentativas = tentativas + 1
-    palpite = tonumber(io.read())
-    print(tentativas)
+   tentativas = tentativas + 1
+   palpite = tonumber(io.read())
+   print(tentativas)
 
-    if palpite == num then
-        break
-    elseif palpite < num then
-        os.execute("clear")
-        print(string.format("(%d) Palpite baixo, tente novamente", palpite))
-    elseif palpite > num then
-        os.execute("clear")
-        print(string.format("(%d) Palpite alto, tente novamente", palpite))
-    end
+   if palpite == num then
+      break
+   elseif palpite < num then
+      os.execute("clear")
+      print(string.format("(%d) Palpite baixo, tente novamente", palpite))
+   elseif palpite > num then
+      os.execute("clear")
+      print(string.format("(%d) Palpite alto, tente novamente", palpite))
+   end
 end
 
 if palpite == num then
-    print(string.format("Voce acertou em %d tentativas, parabens!", tentativas))
+   print(string.format("Voce acertou em %d tentativas, parabens!", tentativas))
 else
-    print(string.format("Nao foi dessa vez... tente novamente"))
+   print(string.format("Nao foi dessa vez... tente novamente"))
 end
